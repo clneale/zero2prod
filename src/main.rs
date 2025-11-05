911fbad1aeed3ec8996b733715595c2bc6d2ae79
Chris Neale <chris.neale@functionallogix.com>
2025-11-05 12:17:07 +1000

    feature: added '/health_check' endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+//! src/main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run()
         .await
