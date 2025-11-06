b837d41b9f8e2606a24c3c04e6e062daf94a9a6a
Chris Neale <chris.neale@functionallogix.com>
2025-11-07 06:19:34 +1000

    chore: instrumented subscriptions endpoint with logging - outputting to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // We are falling back to printing all logs at info level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
