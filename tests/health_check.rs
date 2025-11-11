3fba464840af74465d7b467bae5163e263564123
Chris Neale <chris.neale@functionallogix.com>
2025-11-12 07:36:06 +1000

    chore: added configuration for ssl_mode to database settings

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -137,10 +137,9 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         password: Secret::new("password".to_string()),
         ..config.clone()
     };
-    let mut connection =
-        PgConnection::connect(&maintenance_settings.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let mut connection = PgConnection::connect_with(&maintenance_settings.connection_options())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
@@ -148,7 +147,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.connection_options())
         .await
         .expect("Failed to connect to Postgres.");
 
