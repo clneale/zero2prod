edd83a21de3bc4f77c6b43fd9854da790f518fba
Chris Neale <chris.neale@functionallogix.com>
2025-11-12 10:06:03 +1000

    feature: added SubscriberEmail new type invariant

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -3,4 +3,5 @@ mod subscriber_email;
 mod subscriber_name;
 
 pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
 pub use new_subscriber::NewSubscriber;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,7 +4,7 @@ use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -21,6 +21,11 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    
     let name = match SubscriberName::parse(form.0.name) {
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
@@ -28,7 +33,7 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     // `web::Form` is wrapper around `FormData`
     // `form.0` gives us access to the underlying `FormData`
     let new_subscriber = NewSubscriber {
-        email: form.0.email,
+        email,
         name,
     };
 
@@ -55,7 +60,7 @@ pub async fn insert_subscriber(
             values ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now(),
     )
